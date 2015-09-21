a2be058320e4eaf113235b065b9a671e0a904c92
frnd <fgo@outlook.com>
2015-09-21 09:08:07 +0200

    add some test

--- test/index.js
+++ test/index.js
@@ -4,28 +4,34 @@
 var assert = require('assert');
 var connect = require('connect');
 var winston = require('winston');
+var util = require('util');
 var request = require('supertest');
 var requestLogger = require('../lib');
+
+// Create a test transport that will be used for test the logger.
+var TestTransport = function() {};
+util.inherits(TestTransport, winston.Transport);
+TestTransport.prototype.name = 'TestTransport';
+TestTransport.prototype.log = function(level, msg, meta, done) {
+  done(null);
+};
+
+winston.add(TestTransport);
+winston.remove(winston.transports.Console);
 
 describe('winston-express-logger', function() {
   it('should create a logger in the request', function(done) {
 
     // Bootstrap our environment
-    var logger = new(winston.Logger)({
-      transports: [
-        new(winston.transports.Console)()
-      ]
-    });
     var app = connect();
 
     // Instantiate our Winston logger as middleware.
-    app.use(requestLogger.create(logger));
+    app.use(requestLogger.create(winston));
 
     // Add a special middleware to test the request has the logger object.
     app.use(function(req, res, next) {
-      // TODO check logger exist in the request
-      // TODO check logger type
-      next();
+      // check logger exist in the request
+      return req.logger ? next() : next(new Error('The logger object is not pressent in the request.'));
     });
 
     // Use Connect's static middleware so we can make a dummy request.
@@ -34,70 +40,53 @@ describe('winston-express-logger', function() {
     // Make our dummy request.
     request(app)
       .get(__filename.replace(__dirname, ''))
-      .end(function(err) {
-        done(err);
-      });
+      .expect(200, done);
   });
 
-  it('should log using the logger in the request', function(done) {
+  it('should log the request', function(done) {
+
+    // Complete the test in the transport
+    TestTransport.prototype.log = function(level, msg, meta, callback) {
+      assert.equal(msg, 'End request', 'Incorrect message');
+      assert.equal(meta.method, 'GET', 'Incorrect method');
+      assert.equal(meta.url.path, '/index.js', 'Incorrect path');
+      assert(meta.requestId, 'No request Id');
+      callback(null);
+      done(null);
+    };
 
     // Bootstrap our environment
-    var logger = new(winston.Logger)({
-      transports: [
-        new(winston.transports.Console)()
-      ]
-    });
     var app = connect();
 
     // Instantiate our Winston logger as middleware.
-    app.use(requestLogger.create(logger));
-
-    // Add a special middleware to test the request has the logger object.
-    app.use(function(req, res, next) {
-      req.logger.info('a message');
-      next();
-    });
+    app.use(requestLogger.create(winston));
 
     // Use Connect's static middleware so we can make a dummy request.
     app.use(connect.static(__dirname));
-
-    // Winston emits a `logged` event, so we will listen for when our
-    // middleware actual logs the event so we can test it.
-    logger.once('logged', function(level, message, data) {
-      //TODO
-      console.log("A logged event", data);
-    });
 
     // Make our dummy request.
     request(app)
       .get(__filename.replace(__dirname, ''))
       .end(function(err) {
-        done(err);
+        if(err) {
+          done(err);
+        }
       });
   });
 
-  it('should add an unique id for each request', function(done) {
-    // TODO
-    done();
-  });
+  it('should add an unique id for each request');
 
-  it('should log with a message', function(done) {
-    // TODO
-    done();
-  });
+  it('should log with a non parametrized message');
 
-  it('should log with a parametrized message', function(done) {
-    // TODO
-    done();
-  });
+  it('should log with a parametrized message');
 
-  it('should log with a parametrized message and a callback', function(done) {
-    // TODO
-    done();
-  });
+  it('should log with a parametrized message and metadata');
 
-  it('should log with a message without parameters and a callback', function(done) {
-    // TODO
-    done();
-  });
+  it('should log with a parametrized message, metadata and a callback');
+
+  it('should log with a non parametrized message and a callback');
+
+  it('should profle');
+
+  it('should startTimeout');
 });
